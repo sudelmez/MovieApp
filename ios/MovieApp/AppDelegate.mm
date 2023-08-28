504d6c4026d86ce7e7f6507b33c64b7ee178d0d9
Sude Ölmez <98457237+sudelmez@users.noreply.github.com>
2023-08-28 15:41:26 +0300

    firebase configuration

--- ios/MovieApp/AppDelegate.mm
+++ ios/MovieApp/AppDelegate.mm
@@ -1,7 +1,6 @@
 #import "AppDelegate.h"
-
 #import <React/RCTBundleURLProvider.h>
-
+#import <FirebaseCore/FirebaseCore.h>
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -10,7 +9,7 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-
+  [FIRApp configure];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
